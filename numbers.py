9727508cc9c787d2d1fd89bb7de26eeca7060532
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-19 17:02:23 +0530

    add initial numbers.py script with basic arithmetic operations

---
+++ numbers.py
@@ -0,0 +1,14 @@
+print("Hi")
+print(50 - 5*6)
+print((50 - 5*6) / 4)
+print(8 / 5) # classic division returns a float
+print(17 // 3) # floor division discards the fractional part
+print(17 % 3)# the % operator returns the remainder of the division
+
+
+tax = 12.5 / 100
+price = 100.50
+price * tax
+print(price + _) #In interactive mode, the last printed expression is assigned to the variable _
+print(round(_, 2))
+
