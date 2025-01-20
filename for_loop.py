7b51ac43160c9ba614ea62bddc7d82cb7d87ec6a
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-20 15:31:23 +0530

    add prime number detection example to for_loop.py

--- for_loop.py
+++ for_loop.py
@@ -5,3 +5,12 @@ for w in words:
 for i in range(5):
     print(i)
 
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
