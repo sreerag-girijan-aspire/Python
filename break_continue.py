584beb751010b73764351480e423ad3513e3676e
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-20 15:25:19 +0530

    add break_continue.py script with examples of break and continue statements in loops

---
+++ break_continue.py
@@ -0,0 +1,12 @@
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(f"{n} equals {x} * {n//x}")
+            break
+
+
+for num in range(2, 10):
+    if num % 2 == 0:
+        print(f"Found an even number {num}")
+        continue
+    print(f"Found an odd number {num}")
