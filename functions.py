9fa092823c84e1098bade2f5119da6ef426c45ec
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-20 17:24:17 +0530

    add parrot function with example usage to functions.py

--- functions.py
+++ functions.py
@@ -23,3 +23,11 @@ def ask_ok(prompt, retries=4, reminder='Please try again!'):
         print(reminder)
 
 ask_ok('yes||no  ')
+
+
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
+parrot(voltage=1000000, action='VOOOOOM')             # 2 keyword arguments
