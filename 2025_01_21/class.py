22ab76a6054bd496247fd9e07279c929582b768b
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-21 16:21:03 +0530

    add Dog class with instance variables and methods for managing tricks

--- 2025_01_21/class.py
+++ 2025_01_21/class.py
@@ -17,3 +17,26 @@ class Complex:
 
 x = Complex(3.0, -4.5)
 print(x.r, x.i)
+
+class Dog:
+
+    kind = 'canine'         # class variable shared by all instances
+
+    def __init__(self, name):
+        self.name = name    # instance variable unique to each instance
+
+d = Dog('Fido')
+print(d.name,d.kind)
+
+
+class Dog:
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+d = Dog('Buddy')
+d.add_trick('roll over')
+print(d.tricks)
