b991a5e96832ca298d632cfc3f60c536769343ef
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-22 17:34:50 +0530

    add divide function with error handling for division and zero division cases

--- 2025_01_22/error_handling.py
+++ 2025_01_22/error_handling.py
@@ -4,3 +4,16 @@ while True:
         break
     except ValueError:
         print("Oops!  That was no valid number.  Try again...")
+
+
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("division by zero!")
+    else:
+        print("result is", result)
+    finally:
+        print("executing finally clause")
+
+divide(2, 1)
