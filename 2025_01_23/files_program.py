142ee90a7072bf259c6eced6ff3632b13ddfbaf1
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-23 15:32:28 +0530

    add error handling for file write operations to manage permission errors

--- 2025_01_23/files_program.py
+++ 2025_01_23/files_program.py
@@ -3,3 +3,10 @@ try:
         content = file.read()
 except FileNotFoundError:
     print("The file was not found.")
+
+
+try:
+    with open('protected_file.txt', 'w') as file:
+        file.write("Trying to write to a protected file.")
+except PermissionError:
+    print("You do not have permission to write to this file.")
