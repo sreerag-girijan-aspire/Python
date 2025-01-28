a86530b6a0c32a385da798550912312ae91c5056
sreerag.girijan <sreerag.girijan@aspiresys.com>
2025-01-28 17:47:25 +0530

    add examples of function decorators and their usage

---
+++ 2025_01_28/decorator.py
@@ -0,0 +1,30 @@
+def some_decorator(f):
+    def wraps(*args):
+        print(f"Calling function '{f.__name__}'")
+        return f(args)
+    return wraps
+
+@some_decorator
+def decorated_function(x):
+    print(f"With argument '{x}'")
+
+decorated_function("Hello")
+
+
+# Defining a decorator
+def trace(f):
+    def wrap(*args, **kwargs):
+        print(f"[TRACE] func: {f.__name__}, args: {args}, kwargs: {kwargs}")
+        return f(*args, **kwargs)
+
+    return wrap
+
+# Applying decorator to a function
+@trace
+def add_two(x):
+    return x + 2
+
+# Calling the decorated function
+print(add_two(3))
+# Applying decorator to a lambda
+print((trace(lambda x: x ** 2))(3))
