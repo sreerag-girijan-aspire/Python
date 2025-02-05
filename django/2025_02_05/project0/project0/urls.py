e9d529702cf9db789fbf89f8e371b279ec2cc186
Sreerag EG <sreerag.girijan@aspiresys.com>
2025-02-05 18:05:10 +0530

    add index view and URL routing for app0

--- django/2025_02_05/project0/project0/urls.py
+++ django/2025_02_05/project0/project0/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from project0.views import hello
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("app0.urls")),
     path("hello/",hello)
 ]
