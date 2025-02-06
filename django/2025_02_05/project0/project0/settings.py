732cc37598d33bb1a8e1e5bef83791b4de03c0cc
Sreerag EG <sreerag.girijan@aspiresys.com>
2025-02-06 18:23:06 +0530

    add Question and Choice models with admin registration

--- django/2025_02_05/project0/project0/settings.py
+++ django/2025_02_05/project0/project0/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "app0.apps.App0Config",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "app0",
+    # "app0",
 ]
 
 MIDDLEWARE = [
