993dd03cb69e3ba67edf408b0522d73d564702df
Sreerag EG <sreerag.girijan@aspiresys.com>
2025-02-06 10:49:43 +0530

    update index view response message for clarity

--- django/2025_02_05/project0/app0/views.py
+++ django/2025_02_05/project0/app0/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index page.")
+    return HttpResponse("Hello, world. You're at the index page.!")
 # Create your views here.
