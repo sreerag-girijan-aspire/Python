1543ebefe45891b3eaf2397572e93358b7628aef
Sreerag EG <sreerag.girijan@aspiresys.com>
2025-02-06 11:06:31 +0530

    add third line to string and print joined output

--- string.py
+++ string.py
@@ -1,7 +1,7 @@
 print('spam eggs')
 print('doesn\'t')  # use \' to escape the single quote...
 print("\"Yes,\" they said.")
-s = 'First line.\nSecond line.'  # \n means newline
+s = 'First line.\nSecond line.\nThird line'  # \n means newline
 print(s)
 print(r'C:\some\name')  # note the r before the quote, you can use raw strings by adding an r before
 print("""\
@@ -15,3 +15,5 @@ print(word[0])  # character in position 0
 print(word[-1])  # last character
 print(word[0:2])
 print(word[-2:])  # characters from the second-last (included) to the end
+itr=s.split("\n")
+print("Hello".join(itr))
