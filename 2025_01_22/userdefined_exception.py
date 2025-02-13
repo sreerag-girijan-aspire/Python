575773b1ee3572d599f62dcc71b3019a3f6de284
Sreerag EG <sreerag.girijan@aspiresys.com>
2025-02-13 10:18:38 +0530

    feat: implement user-defined exception with demonstration

--- 2025_01_22/userdefined_exception.py
+++ 2025_01_22/userdefined_exception.py
@@ -1,1 +1,10 @@
-class MyError(Exception): pass
+class MyError(Exception):
+    # def __init__(self, message):
+    #     self.message = message
+    #     super().__init__(self.message)
+    print("This is a user-defined exception")
+
+try:
+    raise MyError()
+except MyError as e:
+    print(e)
