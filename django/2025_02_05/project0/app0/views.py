aaf9336a3796e700a18ac40d0a0fb488c9c98248
Sreerag EG <sreerag.girijan@aspiresys.com>
2025-02-17 10:48:16 +0530

    feat: add ContactFormView and Author management views for creating, updating, and deleting authors

--- django/2025_02_05/project0/app0/views.py
+++ django/2025_02_05/project0/app0/views.py
@@ -35,3 +35,39 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+from app0.forms import ContactForm
+from django.views.generic.edit import FormView
+
+
+class ContactFormView(FormView):
+    template_name = "contact.html"
+    form_class = ContactForm
+    success_url = "/thanks/"
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.send_email()
+        return super().form_valid(form)
+    
+
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from app0.models import Author
+
+
+class AuthorCreateView(CreateView):
+    model = Author
+    fields = ["name"]
+
+
+class AuthorUpdateView(UpdateView):
+    model = Author
+    fields = ["name"]
+
+
+class AuthorDeleteView(DeleteView):
+    model = Author
+    success_url = reverse_lazy("author-list")
