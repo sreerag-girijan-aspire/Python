ead0321cd97282c0c2527a3074bcc146dcabc9c6
Sreerag EG <sreerag.girijan@aspiresys.com>
2025-03-10 15:46:29 +0530

    feat: add addition operator for MyClass to support arithmetic operations

--- 2025_03_10/dunder.py
+++ 2025_03_10/dunder.py
@@ -10,3 +10,17 @@ class MyClass:
 obj = MyClass("Alice")
 print(repr(obj))  # Output: MyClass('Alice')
 print(obj)
+
+
+
+class MyClass:
+    def __init__(self, value):
+        self.value = value
+    
+    def __add__(self, other):
+        return MyClass(self.value + other.value)
+
+obj1 = MyClass(10)
+obj2 = MyClass(20)
+result = obj1 + obj2
+print(result.value)  # Output: 30
