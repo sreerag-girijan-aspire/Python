faeba657ed8546a91e698ac5003a79acc7f5abab
Sreerag EG <sreerag.girijan@aspiresys.com>
2025-03-10 18:16:16 +0530

    feat: add examples for built-in type conversion functions

---
+++ 2025_03_10/builtin_type_conversion.py
@@ -0,0 +1,20 @@
+# int()
+print(int("10"))  
+
+# float()
+print(float("10.5"))  
+
+# str()
+print(str(123))  
+
+# list()
+print(list("hello")) 
+
+# tuple()
+print(tuple([1, 2, 3]))  
+
+# set()
+print(set([1, 2, 2, 3]))  
+
+# dict()
+print(dict([("a", 1), ("b", 2)]))  
