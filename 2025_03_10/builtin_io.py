3052201cd8513cf08f452e32049938c832bf13a6
Sreerag EG <sreerag.girijan@aspiresys.com>
2025-03-10 18:28:22 +0530

    feat: add examples for built-in I/O functions

---
+++ 2025_03_10/builtin_io.py
@@ -0,0 +1,13 @@
+# print()
+print("Hello, world!")
+
+# input()
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+
+# repr()
+print(repr("Hello\nWorld"))  # Returns "'Hello\nWorld'"
+
+# eval()
+result = eval("3 + 4 * 2")  # Evaluates the string as Python code
+print(result)  # Returns 11
